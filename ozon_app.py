1d407ca7e73146cde29500c919f319cb7ccb0af2
YumagulovA <zico.13288@gmail.com>
2020-07-13 16:27:35 +0300

    1.15

--- ozon_app.py
+++ ozon_app.py
@@ -5,7 +5,7 @@ import json
 class ApiClass:
     """ Класс для работы с OzonApi """
 
-    def __init__(self, url, client_id, api_key):
+    def __init__(self, url: str, client_id: int, api_key: str):
 
         self.client_id = client_id
         self.api_key = api_key
@@ -19,7 +19,7 @@ class ApiClass:
 
         self.headers = headers
 
-    def _response(self, func_url, data):
+    def _response(self, func_url: str, data: dict):
 
         r = requests.post(f'https://{self.url}{func_url}', headers=self.headers, data=json.dumps(data))
         if r.status_code == 200:
@@ -27,7 +27,7 @@ class ApiClass:
         else:
             return "Error"
 
-    def category_tree(self, category_id):
+    def category_tree(self, category_id: int) -> dict:
         """ Получение категорий в виде дерева.
         Создание товаров доступно только в категориях последнего уровня, соответственно вам необходимо
         сопоставить именно эти категории с категориями своей площадки.Категории не создаются
@@ -41,7 +41,7 @@ class ApiClass:
 
         return self._response(func_url, data)
 
-    def category_atribute(self, category_id):
+    def category_atribute(self, category_id: int) -> dict:
         """ Возвращает список характеристик категории по ее идентификатору.
         У некоторых категорий есть системные характеристики, которые скрыты от пользователя,
         но по ним товары объединяются в группы. Например, "Название модели" для категорй "Одежда" и "Обувь" """
@@ -60,11 +60,11 @@ class ApiClass:
 
         func_url = "/v2/product/import"
 
-        data = {}
+        data = {'items': [{'category_id': 17028760, 'price': 'old_price', 'vat': '0.2', 'vendor': 'MOTUL', 'dimension_unit': 'mm', 'weight_unit': 'g', 'height': 130, 'depth': 230, 'width': 90, 'images': [{'file_name': 'https://motul.store/upload/resize_cache/iblock/8f5/700_700_1/6100-SYN-CLEAN-5W30-1L.png', 'defaut': True}], 'attributes': [{'id': 7194, 'value': '1'}, {'id': 8084, 'value': '1095'}, {'id': 9048, 'value': '6100 SYN-CLEAN 5W30'}, {'id': 4382, 'value': ' x  x '}, {'id': 4383, 'value': ''}, {'id': 4386, 'collection': ['96']}, {'id': 4389, 'value': '10'}, {'id': 7206, 'collection': ['7']}, {'id': 7290, 'collection': '2'}, {'id': 7284, 'value': '25'}, {'id': 7286, 'value': '4'}], 'weight': 980, 'offer_id': '107947', 'old_price': '666', 'name': 'Моторные масла Motul 6100 SYN-CLEAN 5W30'}]}
 
-        return (self._response(func_url, data))['result']['task_id']
+        return self._response(func_url, data)
 
-    def import_status(self, task_id):
+    def import_status(self, task_id: int) -> int:
         """ Статус добавления товара """
 
         func_url = "/v1/product/import/info"
@@ -75,7 +75,7 @@ class ApiClass:
 
         return self._response(func_url, data)
 
-    def product_static(self, product_id, sku):
+    def product_static(self, product_id: int, sku: int) -> dict:
         """ Возвращает информацию о товаре по его идентификатору. Если указать только offer_id,
         то в результатах поиска будут только товары вашего магазина,
         даже если у другого продавца есть такие же артикулы """
@@ -85,5 +85,18 @@ class ApiClass:
         data = {"offer_id": " ",
                 "product_id": product_id,
                 "sku": sku}
+
+        return self._response(func_url, data)
+    
+    def product_info_list(self, product_id):
+        """ Позволяет получить список товаров. """
+
+        func_url = '/v1/product/list'
+        data = {"filter":
+                {
+                    "offer_id": ["1255959"],
+                    "product_id": [product_id],
+                    "visibility": "ALL"
+                }, }
 
         return self._response(func_url, data)
